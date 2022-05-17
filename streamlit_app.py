4817d467b20ca642bd1e561cc7135abfb53d86b7
mehrdad mohammadian <40211374+mehrdad-dev@users.noreply.github.com>
2022-05-17 16:27:13 +0430

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,71 @@ import torch
 import clip
 import cv2
 import os
+# ================================================================================================
 
 OBJDETECTIONREPO = 'ultralytics/yolov5'
 DEVICE = 'cpu'
 N = 5
+
+def objectDetection(img, model) -> tuple():
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = model(image)
+    croped_objects = result.crop(save=False)
+    
+    listOfObjects = []
+    for obj in croped_objects:
+        listOfObjects.append(cv2.cvtColor(obj['im'], cv2.COLOR_BGR2RGB))
+
+    detectedObjects = result.render()[0]
+
+    return listOfObjects, detectedObjects
+
+
+def similarity_top(similarity_list:list, listOfObjects:list, N) -> tuple():
+    results = zip(range(len(similarity_list)), similarity_list)
+    results = sorted(results, key=lambda x: x[1], reverse=True)
+    images = []
+    scores=[]
+    for index, score in results[:N]:
+        scores.append(score)
+        images.append(listOfObjects[index])
+
+    return scores, images
+
+
+def findObjects(listOfObjects:list, query:str, model, preprocess, device:str, N) -> tuple():
+    objects = torch.stack([preprocess(Image.fromarray(im)) for im in listOfObjects]).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(objects)
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+
+        text_features = model.encode_text(clip.tokenize(query).to(device))
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    # Retrieve the description vector and the photo vectors
+    # @: https://docs.python.org/3/whatsnew/3.5.html?highlight=operator#whatsnew-pep-465
+    similarity = (text_features.cpu().numpy() @ image_features.cpu().numpy().T) * 100
+    similarity = similarity[0]
+    scores, images = similarity_top(similarity, listOfObjects, N=N)     
+
+    return scores, images
+
+
+def pipeline(image, query, models):
+    print(models)
+    listOfObjects, detectedObjects = objectDetection(image, models[0])
+    scores, images = findObjects(listOfObjects, query, models[1], models[2], DEVICE, N)
+    detectedObjects = np.array(detectedObjects)
+
+    st.title('Detected Objects:')
+    st.image(detectedObjects, caption='Detected Objects', use_column_width=True)
+    
+    st.title('Finded Objects:')
+    for index, img in enumerate(images):
+        img = np.array(img)
+        st.image(img, caption="Score: "+str(scores[index]))
+
 
 # ================================================================================================
 st.title('🔍 Search Between the Objects')
@@ -63,70 +124,6 @@ try:
 except:
     print('[LOG]  model load exception')
 # ================================================================================================
-
-
-def objectDetection(img, model) -> tuple():
-    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result = model(image)
-    croped_objects = result.crop(save=False)
-    
-    listOfObjects = []
-    for obj in croped_objects:
-        listOfObjects.append(cv2.cvtColor(obj['im'], cv2.COLOR_BGR2RGB))
-
-    detectedObjects = result.render()[0]
-
-    return listOfObjects, detectedObjects
-
-
-def similarity_top(similarity_list:list, listOfObjects:list, N) -> tuple():
-    results = zip(range(len(similarity_list)), similarity_list)
-    results = sorted(results, key=lambda x: x[1], reverse=True)
-    images = []
-    scores=[]
-    for index, score in results[:N]:
-        scores.append(score)
-        images.append(listOfObjects[index])
-
-    return scores, images
-
-
-def findObjects(listOfObjects:list, query:str, model, preprocess, device:str, N) -> tuple():
-    objects = torch.stack([preprocess(Image.fromarray(im)) for im in listOfObjects]).to(device)
-
-    with torch.no_grad():
-        image_features = model.encode_image(objects)
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-
-        text_features = model.encode_text(clip.tokenize(query).to(device))
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-
-    # Retrieve the description vector and the photo vectors
-    # @: https://docs.python.org/3/whatsnew/3.5.html?highlight=operator#whatsnew-pep-465
-    similarity = (text_features.cpu().numpy() @ image_features.cpu().numpy().T) * 100
-    similarity = similarity[0]
-    scores, images = similarity_top(similarity, listOfObjects, N=N)     
-
-    return scores, images
-
-
-def pipeline(image, query):
-    global models
-    print(models)
-    listOfObjects, detectedObjects = objectDetection(image, models[0])
-    scores, images = findObjects(listOfObjects, query, models[1], models[2], DEVICE, N)
-    detectedObjects = np.array(detectedObjects)
-
-    st.title('Detected Objects:')
-    st.image(detectedObjects, caption='Detected Objects', use_column_width=True)
-    
-    st.title('Finded Objects:')
-    for index, img in enumerate(images):
-        img = np.array(img)
-        st.image(img, caption="Score: "+str(scores[index]))
-
-# # ================================================================================================
-
 uploaded_file = st.file_uploader("Upload a jpg image", type=["jpg"])
 image = 0
 if uploaded_file is not None:
@@ -144,5 +141,5 @@ query = st.text_input('Search Query:')
 left_column, right_column = st.columns(2)
 pressed = left_column.button('Search!')
 if pressed:
-    pipeline(image, query)
+    pipeline(image, query, models)
     st.balloons()
