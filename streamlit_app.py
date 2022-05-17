b5e711a0098502c865e83036bd192d3972827cee
mehrdad mohammadian <40211374+mehrdad-dev@users.noreply.github.com>
2022-05-17 16:33:10 +0430

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,11 +102,10 @@ st.write('You selected:', FINDERMODEL)
 # ================================================================================================
 left_column, right_column = st.columns(2)
 go = left_column.button('Load Models!')
-models = []
 if go:
     @st.experimental_singleton
     def get_model_session(OBJDETECTIONREPO, OBJDETECTIONMODEL, FINDERMODEL, DEVICE):
-        global models
+        models = []
 
         objectDetectorModel = torch.hub.load(OBJDETECTIONREPO, OBJDETECTIONMODEL)
         objectFinderModel, preProcess = clip.load(FINDERMODEL, device=DEVICE)
