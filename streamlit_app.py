6706a13ff72db5fb2c69daeda889c08480797cef
mehrdad mohammadian <40211374+mehrdad-dev@users.noreply.github.com>
2022-05-17 11:35:34 +0430

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,6 +103,7 @@ models = []
 if go:
     @st.experimental_singleton
     def get_model_session(OBJDETECTIONREPO, OBJDETECTIONMODEL, FINDERMODEL, DEVICE):
+        global models
         objectDetectorModel = torch.hub.load(OBJDETECTIONREPO, OBJDETECTIONMODEL)
         objectFinderModel, preProcess = clip.load(FINDERMODEL, device=DEVICE)
         models.append(objectDetectorModel)
