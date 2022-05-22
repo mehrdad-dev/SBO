aaa4d70f8fc65bd6e42c6cf55b3e9243aed41ff2
mehrdad mohammadian <40211374+mehrdad-dev@users.noreply.github.com>
2022-05-22 10:04:33 +0430

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,6 @@ def findObjects(listOfObjects:list, query:str, model, preprocess, device:str, N)
 
 
 def pipeline(image, query, models):
-    # print('======TESTING======')
-    # print(models)
     listOfObjects, detectedObjects = objectDetection(image, models[0])
     scores, images = findObjects(listOfObjects, query, models[1], models[2], DEVICE, N)
     detectedObjects = np.array(detectedObjects)
